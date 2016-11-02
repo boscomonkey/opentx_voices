d4d8ba3117291eb423b2457bcdc30a8e1790c86a
Bosco So <bso@alum.mit.edu.nospam>
2016-11-02 10:01:06 -0700

    sends voice tracks output to output directory (and git-ignoring it)

--- taranis_voices.rb
+++ taranis_voices.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/env ruby
 
+# Quick & dirty script to generate voice tracks for OpenTX voice
+# tracks on a Macintosh. Requires ffmpeg to be installed, and that can
+# be easily accomplished via Homebrew.
+
 require 'csv'
 
 aiff_topdir = '/tmp/taranis_voices'
@@ -17,10 +21,11 @@ csv.each do |path, file, phrase|
   system("say -o #{aiff_file} #{phrase}")
   puts aiff_file
 
-  `mkdir -p #{path}`
-  wav_file = "#{path}/#{file}"
+  output_path = "output/#{path}"
+  `mkdir -p #{output_path}`
+  wav_file = "#{output_path}/#{file}"
   puts wav_file
 
-  system("ffmpeg -i #{aiff_file} -ar 16000 #{wav_file}")
+  system("ffmpeg -y -i #{aiff_file} -ar 16000 #{wav_file}")
   puts
 end
