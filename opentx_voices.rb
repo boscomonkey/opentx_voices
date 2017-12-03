48bbff1b64881075b7948b98cd4c5f2d28e9ad62
Bosco So <bso@alum.mit.edu.nospam>
2017-12-02 16:11:26 -0800

    Betaflight 3.2 sounds
    - added additional tracks to betaflight.csv
    - reorganized filenames

--- opentx_voices.rb
+++ opentx_voices.rb
@@ -7,30 +7,49 @@
 require 'csv'
 require 'fileutils'
 
-# grab the CSV filename from first command line arg
-csv_fname = ARGV[0]
+# each semicolon separated file should be named on the command line
+if ARGV.length == 0
+  STDERR.puts <<EOF
+
+Usage: #{$0} CSVFILE0 [CSVFILE1 ... [CSVFILEn]]
+
+Script to generate voice tracks for OpenTX voice tracks on a
+Macintosh.
+
+Name each semicolon-separated file on the command line. If there are
+output .WAV filename collisions, later CSV files on the command line
+override earlier ones.
+
+Requires ffmpeg to be installed (can be accomplished via Homebrew).
+
+EOF
+  exit -1
+end
 
 # grab voice from environment variable VOICE
 voice_personality = ENV['VOICE']
 voice_arg = voice_personality.nil? ? "" : "-v #{voice_personality}"
 if voice_personality.nil?
-  system("say Using default voice personality")
+  system("default voice")
 else
   system("say #{voice_arg} Using voice personality: #{voice_personality}") or exit 1
 end
 
-# cycle through each line of the CSV using semi-colon as the column separator
-csv = CSV.open(csv_fname, col_sep:';')
-csv.each do |path, file, phrase|
-  next if path.nil? || file.nil? || phrase.nil?
-  puts phrase
+# For each semi-colon separated file, cycle through each line of the
+# CSV using semi-colon as the column separator
+ARGV.each do |csv_fname|
+  csv = CSV.open(csv_fname, col_sep:';')
+  csv.each do |path, file, phrase|
+    next if path.nil? || file.nil? || phrase.nil?
+    puts phrase
 
-  output_path = "output/#{voice_personality}/#{path}"
-  FileUtils.mkpath(output_path) unless Dir.exists?(output_path)
+    output_path = "output/#{voice_personality}/#{path}"
+    FileUtils.mkpath(output_path) unless Dir.exists?(output_path)
 
-  wav_file = "#{output_path}/#{file}"
-  system("say #{voice_arg} -o #{wav_file} --data-format=LEI16@32000 #{phrase}")
-  puts wav_file
+    wav_file = "#{output_path}/#{file}"
+    system("say #{voice_arg} -o #{wav_file} --data-format=LEI16@32000 #{phrase}")
+    puts wav_file
 
-  puts
+    puts
+  end
 end
