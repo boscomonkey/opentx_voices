433ec58f6ff14245f3dc4bcdb43839f04fd43cdb
Bosco So <bso@alum.mit.edu.nospam>
2017-12-04 13:19:32 -0800

    create zip bundles for release

--- bin/generate_voices.sh
+++ bin/generate_voices.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+# Run this script from the top of the repository to generate a zip
+# file for each voice in voices.txt.
+
 BIN=$0
 BINDIR=$(dirname "$BIN")
 VOICES_FILE="$BINDIR/voices.txt"	# assume voices.txt in same directory
 
 for vv in $(cat "$VOICES_FILE"); do
     VOICE="$vv" ./opentx_voices.rb english-taranis.csv betaflight.csv
+
+    # zip only the SOUNDS directory from within the voice directory
+    pushd output/"$vv"
+    zip -9r ../"$vv".zip SOUNDS
+    popd
 done
